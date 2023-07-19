daee25fe23e72e4b2c42d685952f3f624f94744b
sueshin <sueshin@com2us.com>
2023-07-19 18:43:17 +0900

    캠페인 스케쥴러 기능 추가

--- ManagingTool/ManagingTool/Client/Program.cs
+++ ManagingTool/ManagingTool/Client/Program.cs
@@ -2,6 +2,8 @@ using ManagingTool.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
+using Radzen;
+
 
 /*
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -20,7 +22,13 @@ builder.Services.AddSingleton<ItemService>();
 builder.Services.AddSingleton<MailService>();
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddAntDesign();
-builder.Services.AddBlazoredLocalStorage();
+//builder.Services.AddSingleton<DialogService>();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+
+//builder.Services.AddBlazoredLocalStorage();
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
